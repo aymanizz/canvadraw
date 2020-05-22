5e887b61ea9194b833da1f1ed4304acb5e2a176f
Ayman Izzeldin <ayman.izzeldin@gmail.com>
2020-05-22 20:46:30 +0200

    drawing context abstract class

---
+++ src/drawing_context.ts
@@ -0,0 +1,44 @@
+import Konva from "konva"
+
+export default abstract class DrawingContext {
+    readonly layers: Konva.Layer[]
+    readonly currentLayerIndex = 0
+
+    readonly properties = {
+        fill: '#000000',
+        stroke: '#000000',
+        strokeWidth: 2,
+        hitStrokeWidth: 5,
+    }
+
+    constructor(
+        readonly stage: Konva.Stage
+    ) {
+        const layers = stage.getLayers().toArray() as Konva.Layer[]
+        if (layers.length !== 0)
+            this.layers = layers
+        else
+            this.layers = [new Konva.Layer()]
+    }
+
+    get currentLayer() { return this.layers[this.currentLayerIndex] }
+
+    pushLayer(layer: Konva.Layer) {
+        this.layers.push(layer)
+    }
+
+    popLayer(index: number) {
+        return this.layers.splice(index, 1)
+    }
+
+    get pointerPosition() {
+        const position = this.stage.position()
+        const pointer = this.stage.getPointerPosition()!
+        const scale = this.stage.scaleX()
+        const mousePointTo = {
+            x: (pointer.x - position.x) / scale,
+            y: (pointer.y - position.y) / scale
+        }
+        return mousePointTo
+    }
+}
