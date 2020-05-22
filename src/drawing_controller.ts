d98f267684e04f7387c7b7218a47f223b20367a1
Ayman Izzeldin <ayman.izzeldin@gmail.com>
2020-05-22 20:49:42 +0200

    drawing context implementation
    add drawing controller

---
+++ src/drawing_controller.ts
@@ -0,0 +1,33 @@
+import Konva from 'konva'
+import Tool from './core/tool'
+import DrawingContext from './drawing_context'
+
+export default class DrawingController extends DrawingContext {
+    readonly layers = [new Konva.Layer()]
+    readonly currentLayerIndex = 0
+
+    constructor(stage: Konva.Stage, private tool: Tool) {
+        super(stage)
+        this.stage.add(this.currentLayer)
+        this.hookMouseEventsListeners()
+        this.hookKeyEventsListeners()
+    }
+
+    private hookMouseEventsListeners() {
+        this.stage.on('wheel', (ev) => this.tool.onMouseWheel(ev))
+        this.stage.on('mouseup touchstart', (ev) => this.tool.onMouseUp(ev))
+        this.stage.on('mousedown touchend ', (ev) => this.tool.onMouseDown(ev))
+        this.stage.on('mousemove touchmove', (ev) => this.tool.onMouseMove(ev))
+    }
+
+    private hookKeyEventsListeners() {
+        window.addEventListener('keydown', (ev) => this.tool.onKeyDown(ev))
+        window.addEventListener('keyup', (ev) => this.tool.onKeyUp(ev))
+    }
+
+    useTool(tool: Tool) {
+        this.tool.deactivate()
+        this.tool = tool
+        this.tool.activate(this)
+    }
+}
