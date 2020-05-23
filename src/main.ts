fcbb8e629bfe8f8463255319b81bd5fd4c9f0b11
Ayman Izzeldin <ayman.izzeldin@gmail.com>
2020-05-23 17:03:07 +0200

    temporary solution for switching between tools

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import Konva from 'konva'
 import DrawingController from './drawing_controller'
 import { CommandExecutor } from './core/command'
 import * as Tool from './tools'
+import ShortcutsRegistry from './core/shortcut'
 
 export const stage = new Konva.Stage({
     container: 'canvas',
@@ -9,8 +10,19 @@ export const stage = new Konva.Stage({
     height: window.innerHeight
 })
 
-const tools = {
-    'pen': new Tool.PenTool(),
-}
+const tools = [new Tool.PenTool(), new Tool.ZoomTool()]
 
-new DrawingController(stage, new CommandExecutor(), tools.pen)
+let currentTool = 0
+
+const controller = new DrawingController(stage, new CommandExecutor(), tools[currentTool])
+
+// temporary solution for switching between tools
+const globalShortcuts = new ShortcutsRegistry()
+    .put('KeyT', 'none', () => {
+        currentTool = (currentTool + 1) % tools.length
+        const tool = tools[currentTool]
+        controller.useTool(tool)
+        console.log(`using tool: ${Object.getPrototypeOf(tool).constructor.name}`)
+    })
+
+window.addEventListener('keyup', (ev) => globalShortcuts.get(ev)?.call(undefined, ev))
