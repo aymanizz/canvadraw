cffef111bf3ab42e78600d22cc96b57a959e591d
Ayman Izzeldin <ayman.izzeldin@gmail.com>
2020-05-29 16:30:53 +0200

    feat(tools): support line mode for pen tool

--- src/tools/pen.ts
+++ src/tools/pen.ts
@@ -6,47 +6,98 @@ import { DrawLineCommand } from "../commands"
 export class PenTool extends Tool {
     private line?: Konva.Line
 
+    // drawing mode flag.
+    // in free drawing mode, points are added to the line as the user moves the mouse across the screen.
+    // in line drawing mode, clicks determine the start and end the line, making straight lines.
+    //     in this mode, the line contains an extra point. onMouseMove replaces this point to display the
+    //     line following the user's mouse movement.
+    private mode: 'free' | 'line' | 'none' = 'none'
+
+    // whether the user has moved the mouse since last onMouseDown call.
+    private get mouseHasMoved() {
+        return this.mode !== 'free' && this.line && this.line.points().length !== 2
+    }
+
     deactivate() {
-        this.endLine()
+        if (this.line)
+            this.endLine()
         super.deactivate()
     }
 
     onMouseDown(_: KonvaEventObject<MouseEvent>) {
-        if (this.line)
+        if (this.line && this.mode !== 'line')
             return false
-        const { x, y } = this.context.getRelativePointerPosition()
-        this.line = new Konva.Line({
-            ...this.context.overlayProperties,
-            globalCompositeOperation: 'source-over',
-            points: [x, y],
-        })
-        this.context.currentLayer.add(this.line).draw()
+
+        if (this.mode === 'none') {
+            const { x, y } = this.context.getRelativePointerPosition()
+            this.line = new Konva.Line({
+                ...this.context.overlayProperties,
+                globalCompositeOperation: 'source-over',
+                points: [x, y],
+            })
+            this.context.currentLayer.add(this.line).draw()
+
+            this.mode = 'free'
+        }
+
         return true
     }
 
     onMouseMove(_: KonvaEventObject<MouseEvent>) {
         if (!this.line)
             return false
+
         const { x, y } = this.context.getRelativePointerPosition()
-        this.line!.points().push(x, y)
-        this.line!.draw()
+        const points = this.line.points()
+
+        if (this.mode === 'free') {
+            points.push(x, y)
+        } else {
+            points.splice(-2, 2, x, y)
+        }
+        this.context.currentLayer.batchDraw()
+
         return true
     }
 
     onMouseUp(_: KonvaEventObject<MouseEvent>) {
-        return this.endLine()
+        if (!this.line)
+            return false
+
+        const points = this.line.points()
+
+        if (!this.mouseHasMoved) {
+            this.mode = 'line'
+        }
+
+        if (this.mode === 'line') {
+            points.push(...points.slice(-2))
+        } else if (this.mode === 'free') {
+            this.endLine()
+        }
+
+        return true
     }
 
     private endLine() {
-        if (!this.line)
-            return false
-        const command = new DrawLineCommand(this.context.currentLayer, {
-            ...this.line!.getAttrs(),
-            ...this.context.properties,
-        })
-        this.line!.destroy()
+        if (this.mode === 'line') {
+            this.line!.points().splice(-2, 2)
+        }
+        const command = new DrawLineCommand(
+            this.context.currentLayer,
+            this.line!.setAttrs({ ...this.context.properties })
+        )
         this.context.executor.execute(command)
+
         this.line = undefined
-        return true
+        this.mode = 'none'
+    }
+
+    onKeyDown(event: KeyboardEvent) {
+        if (event.key === 'Escape' && this.mode === 'line') {
+            this.endLine()
+            return true
+        }
+        return false
     }
 }
